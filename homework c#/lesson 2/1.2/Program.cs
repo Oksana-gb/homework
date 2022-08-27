3ebb07c76c1c8aeb37d3990acfd0b53e703f3119
oksana <miss.okcana-chernova@yandex.ru>
2022-08-27 23:53:09 +0300

    5

--- homework c#/lesson 2/1.2/Program.cs
+++ homework c#/lesson 2/1.2/Program.cs
@@ -1,14 +1,12 @@
-﻿// Console.WriteLine("введите число");
-// int a = int.Parse(Console.ReadLine());
-// if (a>99) 
-// Console.WriteLine($"тертья цифра {a%100}");
-// else if (a<100)
-// Console.WriteLine("третьей цифры нет");
+﻿Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+if (a>99) 
+Console.WriteLine($"тертья цифра {a%10}");
+else if (a<100)
+Console.WriteLine("третьей цифры нет");
 
 
 
-Console.WriteLine("введите число");
-char[] a = Console.ReadLine().ToCharArray();
-Console.WriteLine(a[2]);
-// else if (a<100)
-// Console.WriteLine("третьей цифры нет");
+// Console.WriteLine("введите число");
+// char[] a = Console.ReadLine().ToCharArray();
+// Console.WriteLine(a[2]);
---
+++ homework c#/lesson 2/1.3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("введите число от 1 до 7");
+int a = int.Parse(Console.ReadLine());
+if (a==6 | a==7)
+Console.WriteLine("выходной");
+else
+Console.WriteLine("будний день");
