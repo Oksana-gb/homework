7293a0cd5638b42ea75e8f4825d7a365ad873b26
oksana <miss.okcana-chernova@yandex.ru>
2022-08-27 23:23:00 +0300

    4

---
+++ homework c#/lesson 2/1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Console.WriteLine("введите число");
+// int a = int.Parse(Console.ReadLine());
+// if (a>99) 
+// Console.WriteLine($"тертья цифра {a%100}");
+// else if (a<100)
+// Console.WriteLine("третьей цифры нет");
+
+
+
+Console.WriteLine("введите число");
+char[] a = Console.ReadLine().ToCharArray();
+Console.WriteLine(a[2]);
+// else if (a<100)
+// Console.WriteLine("третьей цифры нет");
