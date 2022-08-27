eec050ed8116856623b2f0f192a2c5417e23f488
oksana <miss.okcana-chernova@yandex.ru>
2022-08-27 22:23:25 +0300

    3

--- homework c#/lesson 2/1.1/Program.cs
+++ homework c#/lesson 2/1.1/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите число трехзначное число");
+int a = int.Parse(Console.ReadLine());
+if (a<1000 & a>99) 
+Console.WriteLine($"вторая цифра {a%100/10}");
+else 
+Console.WriteLine("оно не трехзначное, введи число заново");
