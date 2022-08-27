45a953521372858d48e622b733a0905a233b8db5
oksana <miss.okcana-chernova@yandex.ru>
2022-08-27 21:57:33 +0300

    2

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -1,3 +1,21 @@
-﻿int num = new Random().Next(100, 1000)
-int a = num/10
-Console.WriteLine($"{num} - {a}");
+﻿// int num = new Random().Next(100, 1000);
+// int a = num / 100;
+// int b = num % 10;
+// Console.WriteLine($"{num} - {a}{b}");
+
+// Console.WriteLine("введите число");
+// int a = int.Parse(Console.ReadLine());
+// if (a % 7 == 0 &  a % 23 == 0)
+//     Console.WriteLine("кратно");
+// else
+//     Console.WriteLine("некратно");
+
+Console.WriteLine("введите число 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число 2");
+int b = int.Parse(Console.ReadLine());
+if (a * a == b)
+    Console.WriteLine("число 1 в квадрате равно числу 2");
+else if (b * b == a )
+     Console.WriteLine("число 2 в квадрате равно числу 1");
+else Console.WriteLine("числа не являются квадратом друг дурга");
