871010461122986e41d49fe6da7eed7bf6fe8e8b
oksana <miss.okcana-chernova@yandex.ru>
2022-08-30 18:21:42 +0300

    new hw

--- homework c#/lesson 3/1/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿Console.WriteLine("введите число");
-int a = convert.toint32(console.ReadLine);
-Console.WriteLine(a);
---
+++ homework c#/lesson 3/3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите число");
+int a = int.Parse(Console.ReadLine());
+for (int i =1; i < a+1; i++) 
+{
+double b = Math.Pow(i,3);
+Console.Write($"{b}, ");
+}
