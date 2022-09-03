61dc60d312a7777d6b4f5ce735f9961aca153c63
oksana <miss.okcana-chernova@yandex.ru>
2022-09-03 19:40:40 +0300

    lesson 4

--- homework c#/lesson 4/1/Program.cs
+++ homework c#/lesson 4/1/Program.cs
@@ -1,4 +1,12 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 Console.WriteLine("введите число А");
-int a = int.Parse(console.ReadLine());
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result *= a;
+}
+Console.Write(result);
---
+++ homework c#/lesson 4/2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.WriteLine("введите число");
+int num = int.Parse(Console.ReadLine());
+
+int Count(int numA)
+{
+    int sum = 0;
+    while (numA > 0)
+    {
+        numA /= 10;
+        sum++;
+    }
+    return sum;
+}
+Console.WriteLine(Count(num));
